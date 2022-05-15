97a25c9b3cfbd3dea1f2b0b49ad0de9a80085aea
Lucas Cândido <lucascandido1995@gmail.com>
2022-05-15 14:23:12 -0300

    Step3: Bloc tests

---
+++ lib/bloc/persons_bloc.dart
@@ -0,0 +1,62 @@
+import 'package:bloc_example/bloc/bloc_actions.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc_example/bloc/person.dart';
+import 'package:flutter/foundation.dart' show immutable;
+
+extension IsEqualToIgnoringOrdering<T> on Iterable<T> {
+  bool isEqualToIgnoringOrdering(Iterable<T> other) =>
+      length == other.length &&
+      {...this}.intersection({...other}).length == length;
+}
+
+@immutable
+class FetchResult {
+  final Iterable<Person> persons;
+  final bool isRetrievedFromCache;
+
+  const FetchResult({
+    required this.persons,
+    required this.isRetrievedFromCache,
+  });
+
+  @override
+  String toString() =>
+      'Result (isRetrievedFromCache = $isRetrievedFromCache, person = $persons';
+
+  @override
+  bool operator ==(covariant FetchResult other) =>
+      persons.isEqualToIgnoringOrdering(other.persons) &&
+      isRetrievedFromCache == other.isRetrievedFromCache;
+
+  @override
+  int get hashCode => Object.hash(persons, isRetrievedFromCache);
+}
+
+class PersonsBloc extends Bloc<LoadAction, FetchResult?> {
+  final Map<String, Iterable<Person>> _cache = {};
+  PersonsBloc() : super(null) {
+    on<LoadPersonsAction>(
+      (event, emit) async {
+        final url = event.url;
+        if (_cache.containsKey(url)) {
+          // we have the value in the cache
+          final cachedPersons = _cache[url]!;
+          final result = FetchResult(
+            persons: cachedPersons,
+            isRetrievedFromCache: true,
+          );
+          emit(result);
+        } else {
+          final loader = event.loader;
+          final persons = await loader(url);
+          _cache[url] = persons;
+          final result = FetchResult(
+            persons: persons,
+            isRetrievedFromCache: false,
+          );
+          emit(result);
+        }
+      },
+    );
+  }
+}
